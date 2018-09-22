c854ea8141c4698223d20a13c680894d741a9410
akhilsathuluri <akhilsathuluri@gmail.com>
2018-09-22 13:37:53 +0530

    changes in poly_cont.cpp

--- poly_cont.cpp
+++ poly_cont.cpp
@@ -7,15 +7,13 @@
 
 using namespace std;
 
-// double* polycont(double* f, double* J, double sof_rows_J, double sof_cols_J, double* theta, double* phi);
-double* polycont(double sof_rows_J, double sof_cols_J, double* theta, double* phi);
-double* absarray(double *data, int size);
-double* LinearSolve(double *A_data, int sof_A_rows, int sof_A_cols, double *b_data);
-double** computeJ(double* th, double* ph);
+double* polycont(double sof_rows_J, double sof_cols_J, double* th, double* ph);
+double* absarray(double* data, int size);
+double* LinearSolve(double* A_data, int sof_A_rows, int sof_A_cols, double* b_data);
+double* computeJ(double* th, double* ph);
 double* computef(double* th, double* ph);
 
 int main(){
-
 double* phval;
 double th[3] = {0.13, 1.4242, 2.4314};
 double ph[3] = {-0.134, 0.2452, 1.442652};
@@ -25,28 +23,17 @@ cout<<phval[0]<<" "<<phval[1]<<" "<<phval[2]<<" "<<endl;
 return 0;
 }
 
-
-double* polycont(int sof_rows_J, int sof_cols_J, double *theta, double *phi){
-do{
-  double** J;
-  double* f;
-  J = computeJ(theta, phi);
-  f = computef(theta, phi);
-  for(int a=0;a<sof_cols_J;a++)
-    phi[a]=phi[a]-(LinearSolve(J, sof_rows_J, sof_cols_J, f))[a];
-}while(gsl_stats_max(absarray(f, sof_cols_J), 1, sof_cols_J)<gsl_sf_pow_int(10.0, -12));
-return phi;
+double* polycont(int sof_rows_J, int sof_cols_J, double* th, double* ph){
+  double* J = new double[sof_cols_J*sof_rows_J];
+  double* f = new double[sof_cols_J];
+  do{
+    J = computeJ(th, ph);
+    f = computef(th, ph);
+    for(int a=0;a<sof_cols_J;a++)
+      ph[a]=ph[a]-(LinearSolve(J, sof_rows_J, sof_cols_J, f))[a];
+  }while(gsl_stats_max(absarray(f, sof_cols_J), 1, sof_cols_J)<gsl_sf_pow_int(10.0, -12));
+  return ph;
 }
-//
-// double* polycont(double *f, double *J, int sof_rows_J, int sof_cols_J, double *theta, double *phi){
-// do{
-// J = computeJ(theta, phi);
-// f = computef(theta, phi);
-// for(int a=0;a<sof_cols_J;a++)
-// phi[a]=phi[a]-(LinearSolve(J, sof_rows_J, sof_cols_J, f))[a];
-// }while(gsl_stats_max(absarray(f, sof_cols_J), 1, sof_cols_J)<gsl_sf_pow_int(10.0, -12));
-// return phi;
-// }
 
 double* absarray(double* data, int size){
 for(int i=0;i<size;i = i+1){
@@ -78,22 +65,17 @@ double* LinearSolve(double *A_data, int sof_A_rows, int sof_A_cols, double *b_da
   return x->data;
 }
 
-//Both of these functions work but have a bad habit of returning a location created in local. refer: https://stackoverflow.com/questions/17312336/warning-function-returns-address-of-local-variable-enabled-by-default,  for right way to return such variables
-
-double** computeJ(double* th, double* ph){
-double** tempJ = new double*[3];
-for(int i = 0; i < 3; i++) {
-  tempJ[i] = new double[3];
-}
-tempJ[0][0]= (2*sin(ph[0]))/3. + (2*cos(ph[1])*sin(ph[0]))/9. - (cos(th[0])*sin(ph[0]))/3. + (cos(th[1])*sin(ph[0]))/3. - (2*cos(ph[0])*sin(ph[1]))/9. + (cos(ph[0])*sin(th[0]))/3. - (cos(ph[0])*sin(th[1]))/3.;
-tempJ[0][1] = (-2*cos(ph[1])*sin(ph[0]))/9. - (2*sin(ph[1]))/3. + (2*cos(ph[0])*sin(ph[1]))/9. + (cos(th[0])*sin(ph[1]))/3. - (cos(th[1])*sin(ph[1]))/3. - (cos(ph[1])*sin(th[0]))/3. + (cos(ph[1])*sin(th[1]))/3.;
-tempJ[0][2] = 0;
-tempJ[1][0] = -(cos(ph[0])*pow(3,-0.5)) + sin(ph[0])/3. + (2*cos(ph[2])*sin(ph[0]))/9. - (cos(th[0])*sin(ph[0]))/3. + (cos(th[2])*sin(ph[0]))/3. - (2*cos(ph[0])*sin(ph[2]))/9. + (cos(ph[0])*sin(th[0]))/3. - (cos(ph[0])*sin(th[2]))/3.;
-tempJ[1][1] = 0;
-tempJ[1][2] = cos(ph[2])*pow(3,-0.5) - (2*cos(ph[2])*sin(ph[0]))/9. - sin(ph[2])/3. + (2*cos(ph[0])*sin(ph[2]))/9. + (cos(th[0])*sin(ph[2]))/3. - (cos(th[2])*sin(ph[2]))/3. - (cos(ph[2])*sin(th[0]))/3. + (cos(ph[2])*sin(th[2]))/3.;
-tempJ[2][0] = 0;
-tempJ[2][1] = -(cos(ph[1])*pow(3,-0.5)) - sin(ph[1])/3. + (2*cos(ph[2])*sin(ph[1]))/9. - (cos(th[1])*sin(ph[1]))/3. + (cos(th[2])*sin(ph[1]))/3. - (2*cos(ph[1])*sin(ph[2]))/9. + (cos(ph[1])*sin(th[1]))/3. - (cos(ph[1])*sin(th[2]))/3.;
-tempJ[2][2] = cos(ph[2])*pow(3,-0.5) - (2*cos(ph[2])*sin(ph[1]))/9. + sin(ph[2])/3. + (2*cos(ph[1])*sin(ph[2]))/9. + (cos(th[1])*sin(ph[2]))/3. - (cos(th[2])*sin(ph[2]))/3. - (cos(ph[2])*sin(th[1]))/3. + (cos(ph[2])*sin(th[2]))/3.;
+double* computeJ(double* th, double* ph){
+double* tempJ = new double[9];
+tempJ[0]= (2*sin(ph[0]))/3. + (2*cos(ph[1])*sin(ph[0]))/9. - (cos(th[0])*sin(ph[0]))/3. + (cos(th[1])*sin(ph[0]))/3. - (2*cos(ph[0])*sin(ph[1]))/9. + (cos(ph[0])*sin(th[0]))/3. - (cos(ph[0])*sin(th[1]))/3.;
+tempJ[1] = (-2*cos(ph[1])*sin(ph[0]))/9. - (2*sin(ph[1]))/3. + (2*cos(ph[0])*sin(ph[1]))/9. + (cos(th[0])*sin(ph[1]))/3. - (cos(th[1])*sin(ph[1]))/3. - (cos(ph[1])*sin(th[0]))/3. + (cos(ph[1])*sin(th[1]))/3.;
+tempJ[2] = 0;
+tempJ[3] = -(cos(ph[0])*pow(3,-0.5)) + sin(ph[0])/3. + (2*cos(ph[2])*sin(ph[0]))/9. - (cos(th[0])*sin(ph[0]))/3. + (cos(th[2])*sin(ph[0]))/3. - (2*cos(ph[0])*sin(ph[2]))/9. + (cos(ph[0])*sin(th[0]))/3. - (cos(ph[0])*sin(th[2]))/3.;
+tempJ[4] = 0;
+tempJ[5] = cos(ph[2])*pow(3,-0.5) - (2*cos(ph[2])*sin(ph[0]))/9. - sin(ph[2])/3. + (2*cos(ph[0])*sin(ph[2]))/9. + (cos(th[0])*sin(ph[2]))/3. - (cos(th[2])*sin(ph[2]))/3. - (cos(ph[2])*sin(th[0]))/3. + (cos(ph[2])*sin(th[2]))/3.;
+tempJ[6] = 0;
+tempJ[7] = -(cos(ph[1])*pow(3,-0.5)) - sin(ph[1])/3. + (2*cos(ph[2])*sin(ph[1]))/9. - (cos(th[1])*sin(ph[1]))/3. + (cos(th[2])*sin(ph[1]))/3. - (2*cos(ph[1])*sin(ph[2]))/9. + (cos(ph[1])*sin(th[1]))/3. - (cos(ph[1])*sin(th[2]))/3.;
+tempJ[8] = cos(ph[2])*pow(3,-0.5) - (2*cos(ph[2])*sin(ph[1]))/9. + sin(ph[2])/3. + (2*cos(ph[1])*sin(ph[2]))/9. + (cos(th[1])*sin(ph[2]))/3. - (cos(th[2])*sin(ph[2]))/3. - (cos(ph[2])*sin(th[1]))/3. + (cos(ph[2])*sin(th[2]))/3.;
 
 return tempJ;
 }
