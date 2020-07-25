08037f8dd5e5583c4d8ea93a8f58cb8de33cc544
akhilsathuluri <akhilsathuluri@gmail.com>
2020-07-25 19:28:51 +0530

    added compilation directions

--- multirun.sh
+++ multirun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for (( c=1; c<=10; c++ ))
 do
-  echo "Running $c times"
-  g++ TrackRoot.cpp -lgsl -lgslcblas -lm
+#  echo "Running $c times"
+#  g++ TrackRoot.cpp -lgsl -lgslcblas -lm
+	./what
 done
