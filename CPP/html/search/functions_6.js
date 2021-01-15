3935f87fe277bca34e52b0c87129529587abe147
AdityaKolte17 <adityakolte72@gmail.com>
2021-01-15 10:55:37 +0530

    Added a comment for the manipulate function

--- CPP/html/search/functions_6.js
+++ CPP/html/search/functions_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['s1dist_47',['s1Dist',['../utils_8hh.html#aa55cfeada673b3ce1ce1e504925e117c',1,'utils.hh']]],
   ['savedata_48',['saveData',['../main_8cc.html#ade24902e309127c75974d1e24d715e51',1,'saveData(MatrixXd A, const char file_name[]):&#160;main.cc'],['../timings_8cc.html#ade24902e309127c75974d1e24d715e51',1,'saveData(MatrixXd A, const char file_name[]):&#160;timings.cc']]],
-  ['singularityeventidentifier_49',['SingularityEventIdentifier',['../classRootTracker.html#a1d120fc9c9d777ba405a76c47dfa94e5',1,'RootTracker']]]
+  ['singularityeventidentifier_49',['SingularityEventIdentifier',['../classRootTracker.html#ada1cf0e1ae31392f075fe5f92972f17f',1,'RootTracker']]]
 ];
